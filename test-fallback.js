3e231f4e6c28c7ee5285c84c07a05d5af19b9672
Kevin Martensson <kevinmartensson@gmail.com>
2017-06-13 19:30:24 +0200

    Improve fallback tests

--- test-fallback.js
+++ test-fallback.js
@@ -1,8 +1,12 @@
 import os from 'os';
 import test from 'ava';
 
-// Otherwise, `os.userInfo()` would take precedence over `process.env.LOGNAME`.
+// Remove `os.userInfo()` and reset ENV flags to avoid them taking precedence
 delete os.userInfo;
+process.env.LOGNAME = '';
+process.env.USER = '';
+process.env.LNAME = '';
+process.env.USERNAME = '';
 
 test('async', async t => {
 	t.true((await require('./')()).length > 1);
